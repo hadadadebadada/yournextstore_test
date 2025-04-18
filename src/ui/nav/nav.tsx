bf4302c4e7fb2b77a90a4d3d13495151c5aa6525
artur <artur_pfeifer2@gmx.de>
2025-04-18 15:55:43 +0200

    removed user icon

--- src/ui/nav/nav.tsx
+++ src/ui/nav/nav.tsx
@@ -3,7 +3,7 @@ import { NavMenu } from "@/ui/nav/nav-menu";
 import { SearchNav } from "@/ui/nav/search-nav";
 import { SeoH1 } from "@/ui/seo-h1";
 import { YnsLink } from "@/ui/yns-link";
-import { UserIcon } from "lucide-react";
+//import { UserIcon } from "lucide-react";
 
 export const Nav = async () => {
 	return (
